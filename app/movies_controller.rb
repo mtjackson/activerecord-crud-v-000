676bac875d44ac10e3ca61e86593d2ada7454b16
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-04 12:29:51 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -26,9 +26,9 @@ def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 }
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
-    m.director = args[:director]
-    m.lead = args[:lead]
-    m.in_theaters = args[:in_theaters]
+#    m.director = args[:director]
+#    m.lead = args[:lead]
+#    m.in_theaters = args[:in_theaters]
   end
 end
 
