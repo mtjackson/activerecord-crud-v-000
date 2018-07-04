10fdc0dd25be85b1e450ec9e34eb350f56e5b9e5
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-04 12:17:36 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,7 +28,7 @@ def can_be_created_in_a_block(args = nil)
     m.release_date = args.release_date
     m.director = args.director
     m.lead = args.lead
-    m.in_theaters = ''
+    m.in_theaters = args.in_theaters
   end
 end
 
