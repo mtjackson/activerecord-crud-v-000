a03121459a5c6f0e4070e0bf3a9834e803a0d6ec
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-04 11:49:01 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -70,7 +70,7 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  movie = movies.find_by(title: "Wat?")
+  movie = Movie.find_by(title: "Wat?")
   movie.title = "Wat, huh?"
   movie.save
 end
