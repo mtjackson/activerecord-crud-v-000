113bd1af4c91e7a458184c5cf9dde2f8419aab29
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-04 12:16:11 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,7 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = m)
+def can_be_created_in_a_block(args = attributes)
   Movie.create do |m|
     m.title = ''
     m.release_date = ''
